9db6be0f52b4f28036687d7ccc32388e8aadac22
MBaran88 <zdzibq@gmail.com>
2017-06-01 18:13:10 +0200

    Beginnings

---
+++ src/com/ONP/Algorithm.java
@@ -0,0 +1,19 @@
+package com.ONP;
+
+import java.util.Stack;
+
+/**
+ * Created by RENT on 2017-06-01.
+ */
+public class Algorithm {
+
+    static void push(Stack st, int a) {
+        st.push(new Integer(a));
+        System.out.println("push(" + a + ")");
+        System.out.println("stack: " + st);
+    }
+    static void show(Stack st){
+        
+    }
+
+}
---
+++ src/com/ONP/Main.java
@@ -0,0 +1,10 @@
+package com.ONP;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// trzeba zrobić algorytm ONP który przeliczy wartość wyrażenia 12 1 2 3 * 4 5 / + * +
+
+
+    }
+}
